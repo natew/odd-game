af680b75ffeb2c8de00ef4daa274b2227c81c403
Nate Wienert <nwienert@zappos.com>
2013-12-04 18:00:21 -0800

    Better centering, graphics, working on collision

--- js/odd.js
+++ js/odd.js
@@ -1,36 +1,47 @@
 // Globals
 var container, scene, camera, renderer, controls, stats;
+var SCREEN_WIDTH, SCREEN_HEIGHT;
 var keyboard = new THREEx.KeyboardState();
 var clock = new THREE.Clock();
+var mesh = new THREE.Object3D();
 
-var collidableMeshList = [];
+var colors = {
+  light: 0xFFFFFF,
+  sky: 0x183759,
+  wall: 0x420505,
+  car: 0xd8d600,
+  shell: 0xCC0000
+}
+
+var obstacles = [];
 var cars = [],
     activeCar = 0,
     shells = [],
     numShells = 0;
 
-var darkMaterial = new THREE.MeshBasicMaterial( { color: 0xffffcc } );
-var wireframeMaterial = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, transparent: true } );
-var multiMaterial = [ darkMaterial, wireframeMaterial ];
+var wireframeMaterial = new THREE.MeshLambertMaterial( { color: 0x000000, wireframe: true, transparent: true } );
 
-var SHELL_SPEED = 10;
+var SHELL_SPEED = 20;
 
 init();
 animate();
 
 function init() {
-  var SCREEN_WIDTH = window.innerWidth - 16,
-      SCREEN_HEIGHT = window.innerHeight - 16,
-      VIEW_ANGLE = 45,
+  SCREEN_WIDTH = window.innerWidth;
+  SCREEN_HEIGHT = window.innerHeight;
+
+  var VIEW_ANGLE = 45,
       ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT,
       NEAR = .1,
       FAR = 10000;
 
   // Scene / Camera
   scene = new THREE.Scene();
+  scene.add(mesh);
   camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
   scene.add(camera);
-  camera.position.set(0, 1500, 30);
+
+  camera.position.set(0, (SCREEN_HEIGHT + SCREEN_WIDTH) / 2 + 20, 0);
   camera.lookAt(scene.position);
 
   // Renderer
@@ -48,77 +59,78 @@ function init() {
   // Controls
   controls = new THREE.OrbitControls( camera, renderer.domElement );
 
-  sky();
+  createSky();
+  createWalls();
+  createCars();
+  createFloor();
   lighting();
-  walls();
-  renderCars();
-  floor();
   loadObjects();
 }
 
-function floor() {
-  var floorTexture = new THREE.ImageUtils.loadTexture('images/checkerboard.jpg');
-  floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
-  floorTexture.repeat.set(10, 10);
-  var floorMaterial = new THREE.MeshBasicMaterial({ map:floorTexture, side:THREE.DoubleSide });
-  var floorGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);
+function createFloor() {
+  var floorMaterial = new THREE.MeshLambertMaterial({ color:0xffffff, side:THREE.DoubleSide });
+  var floorGeometry = new THREE.PlaneGeometry(SCREEN_WIDTH, SCREEN_HEIGHT, 10, 10);
   var floor = new THREE.Mesh(floorGeometry, floorMaterial);
   floor.position.y = -0.5;
   floor.rotation.x = Math.PI / 2;
-  scene.add(floor);
+  mesh.add(floor);
 }
 
-function sky() {
+function createSky() {
   var skyBoxGeometry = new THREE.CubeGeometry( 10000, 10000, 10000 );
-  var skyBoxMaterial = new THREE.MeshBasicMaterial( { color: 0x59b6ff, side: THREE.BackSide } );
+  var skyBoxMaterial = new THREE.MeshPhongMaterial( { color: colors['sky'], side: THREE.BackSide } );
   var skyBox = new THREE.Mesh( skyBoxGeometry, skyBoxMaterial );
-  scene.add(skyBox);
+  mesh.add(skyBox);
 }
 
-function walls() {
+function createWalls() {
   var i;
-  var wallWidth = [1000, 1000, 50, 50];
-  var wallLength = [50, 50, 1000, 1000];
-  var wallX = [0, 0, 500, -500];
-  var wallZ = [500, -500, 0, 0];
+  var wallLengthH = SCREEN_WIDTH,
+      wallLengthV = SCREEN_HEIGHT,
+      wallPosH = wallLengthH / 2,
+      wallPosV = wallLengthV / 2,
+      wallWidth = 20,
+      wallHeight = 40;
+
+  var wallWidths = [wallLengthH, wallLengthH, wallWidth, wallWidth];
+  var wallLengths = [wallWidth, wallWidth, wallLengthV, wallLengthV];
+  var wallX = [0, 0, wallPosH, -wallPosH];
+  var wallZ = [wallPosV, -wallPosV, 0, 0];
 
   for (i = 0; i < 4; i++) {
-
+    var wallMaterial = new THREE.MeshPhongMaterial( { color: colors['wall'] } );
     var wall = THREE.SceneUtils.createMultiMaterialObject(
-        new THREE.CubeGeometry(wallWidth[i], 20, wallLength[i], 1, 5, 1),
-        multiMaterial );
-    wall.position.set(wallX[i], 10, wallZ[i]);
-    scene.add( wall );
+        new THREE.CubeGeometry(wallWidths[i], wallHeight, wallLengths[i], 1, 1, 1),
+        [ wallMaterial, wireframeMaterial ] );
+    wall.position.set(wallX[i], wallHeight / 2, wallZ[i]);
+    wall.castShadow = true;
+    wall.receiveShadow = true;
+    obstacles.push(wall);
+    mesh.add( wall );
   }
 }
 
-function renderCars() {
-  var i;
-  for (i = 0; i < 2; i++) {
-    var car = THREE.SceneUtils.createMultiMaterialObject(
-        new THREE.CubeGeometry(80, 50, 50, 1, 1, 1),
-        multiMaterial );
-    car.position.set(100 * i, 25, 0);
-    scene.add(car);
-    cars.push(car);
-  }
-}
-
+var spotLight;
 function lighting() {
   // Point light
-  var pointLight = new THREE.PointLight(0xFFFFFF);
-  pointLight.position.x = 10;
-  pointLight.position.y = 400;
-  pointLight.position.z = 330;
-  scene.add(pointLight);
+  spotLight = new THREE.SpotLight(colors['light'], 5.0);
+  spotLight.position.set(0, 2000, 0);
+  spotLight.castShadow = true;
+  // spotLight.shadowCameraVisible = true;
+  spotLight.shadowMapWidth = 1024 * 2;
+  spotLight.shadowMapHeight = 1024 * 2;
+  spotLight.shadowCameraNear = 500;
+  spotLight.shadowCameraFar = 4000;
+  spotLight.shadowCameraFov = 30;
+  mesh.add(spotLight);
 }
 
 function moveForward(car, moveDistance) {
-  car.position.z += moveDistance;
+  car.position.z -= moveDistance;
 }
 
 function moveBackward(car, moveDistance) {
-  car.position.z -= moveDistance;
+  car.position.z += moveDistance;
 }
 
 function shoot() {
@@ -132,18 +144,34 @@ function shoot() {
 }
 
 // set up the sphere vars
-var radius = 20, segments = 16, rings = 16;
-var sphereMaterial = new THREE.MeshLambertMaterial({ color: 0xCC0000 });
-var sphereGeometry = new THREE.SphereGeometry(radius, segments, rings);
+// var radius = 20, segments = 16, rings = 16;
+var shellMaterial = new THREE.MeshLambertMaterial({ color: colors['shell'] });
+var shellGeometry = new THREE.CubeGeometry(30, 30, 30, 1, 1, 1);
 
 function createShell(position) {
-  var mesh = new THREE.Mesh( sphereGeometry, sphereMaterial )
-  mesh.position.set(position.x, position.y, position.z);
-  mesh.geometry.dynamic = true;
-  mesh.geometry.verticesNeedUpdate = true;
-  mesh.geometry.normalsNeedUpdate = true;
-  scene.add(mesh);
-  return mesh;
+  var shell = new THREE.Mesh( shellGeometry, shellMaterial );
+  shell.position.set(position.x, position.y, position.z);
+  // shell.geometry.dynamic = true;
+  // shell.geometry.verticesNeedUpdate = true;
+  // shell.geometry.normalsNeedUpdate = true;
+  mesh.add(shell);
+  return shell;
+}
+
+function createCars() {
+  var i;
+  for (i = 0; i < 2; i++) {
+    var carMaterial = new THREE.MeshPhongMaterial( { color: colors['car'] } );
+    var car = THREE.SceneUtils.createMultiMaterialObject(
+        new THREE.CubeGeometry(80, 50, 50, 1, 1, 1),
+        [ carMaterial, wireframeMaterial ] );
+    car.position.set((500 * i) - 250, 25, 0);
+    car.castShadow = true;
+    car.receiveShadow = true;
+    obstacles.push(car);
+    mesh.add(car);
+    cars.push(car);
+  }
 }
 
 // Shoot keycode
@@ -158,13 +186,13 @@ function update() {
   var moveDistance = 200 * delta;
   var rotateAngle = Math.PI / 2 * delta; // pi/2 radians (90 degrees) per second
 
-  if ( keyboard.pressed("left") )
+  if (keyboard.pressed("left"))
     cars[activeCar].rotation.y += rotateAngle;
-  if ( keyboard.pressed("right") )
+  if (keyboard.pressed("right"))
     cars[activeCar].rotation.y -= rotateAngle;
-  if ( keyboard.pressed("up") )
+  if (keyboard.pressed("up"))
     moveForward(cars[activeCar], moveDistance);
-  if ( keyboard.pressed("down") )
+  if (keyboard.pressed("down"))
     moveBackward(cars[activeCar], moveDistance);
 
   if (numShells) {
@@ -175,39 +203,18 @@ function update() {
       x = dist * Math.sin(shell.radians);
       y = dist * Math.cos(shell.radians);
 
-      console.log(shell.degree, x, y);
-
       shell.position.x += x;
       shell.position.z += y;
 
+      collisionDetect(shell);
+
       if (++shell.timeElapsed > 100) {
         shells.splice(i, 1);
-        scene.remove(shell);
+        mesh.remove(shell);
         numShells--;
       }
     }
   }
-
-  // // collision detection:
-  // //   determines if any of the rays from the cube's origin to each vertex
-  // //    intersects any face of a mesh in the array of target meshes
-  // //   for increased collision accuracy, add more vertices to the cube;
-  // //    for example, new THREE.CubeGeometry( 64, 64, 64, 8, 8, 8, wireMaterial )
-  // //   HOWEVER: when the origin of the ray is within the target mesh, collisions do not occur
-  // var originPoint = MovingCube.position.clone();
-
-  // clearText();
-
-  // for (var vertexIndex = 0; vertexIndex < MovingCube.geometry.vertices.length; vertexIndex++) {
-  //   var localVertex = MovingCube.geometry.vertices[vertexIndex].clone();
-  //   var globalVertex = localVertex.applyMatrix4( MovingCube.matrix );
-  //   var directionVector = globalVertex.sub( MovingCube.position );
-
-  //   var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
-  //   var collisionResults = ray.intersectObjects( collidableMeshList );
-  //   if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() )
-  //     appendText(" Hit ");
-  // }
 
   controls.update();
   // stats.update();
@@ -220,6 +227,8 @@ function animate() {
 }
 
 function render() {
+  renderer.shadowMapEnabled = true;
+  renderer.shadowMapSoft = true;
   renderer.render(scene, camera);
 }
 
@@ -238,3 +247,82 @@ function loadObjects() {
   //   scene.add(result.scene);
   // });
 }
+
+var limit = 10;
+
+// function collisionDetect(obj) {
+//   var originPoint = obj.position.clone();
+//   for (var vertexIndex = 0; vertexIndex < obj.geometry.vertices.length; vertexIndex++) {
+//     var localVertex = obj.geometry.vertices[vertexIndex].clone();
+//     var globalVertex = localVertex.applyMatrix4( obj.matrix );
+//     var directionVector = globalVertex.sub( obj.position );
+
+//     var ray = new THREE.Raycaster( originPoint, directionVector.clone().normalize() );
+//     var collisionResults = ray.intersectObjects( obstacles );
+//     if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() )
+//       console.log('hit')
+//   }
+// }
+
+var collisions,
+    i,
+    distance = 32,
+    rays = [
+      new THREE.Vector3(0, 0, 1),
+      new THREE.Vector3(1, 0, 1),
+      new THREE.Vector3(1, 0, 0),
+      new THREE.Vector3(1, 0, -1),
+      new THREE.Vector3(0, 0, -1),
+      new THREE.Vector3(-1, 0, -1),
+      new THREE.Vector3(-1, 0, 0),
+      new THREE.Vector3(-1, 0, 1)
+    ],
+    caster = new THREE.Raycaster();
+
+function collisionDetect(obj) {
+  // for each ray
+  for (i = 0; i < rays.length; i++) {
+    // set raycaster position
+    if (limit-- > 0) console.log(obj.position);
+    caster.set(obj.position, rays[i]);
+    // if (i == 0) console.log(caster);
+    // debugger;
+    collisions = caster.intersectObjects(obstacles);
+    if (collisions.length > 0 && collisions[0].distance <= distance) {
+      console.log('colllllide')
+    }
+  }
+}
+
+
+var keys = [37, 38, 39, 40];
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;
+}
+
+function keydown(e) {
+    for (var i = keys.length; i--;) {
+        if (e.keyCode === keys[i]) {
+            preventDefault(e);
+            return;
+        }
+    }
+}
+
+function wheel(e) {
+  preventDefault(e);
+}
+
+function disableScroll() {
+  if (window.addEventListener) {
+      window.addEventListener('DOMMouseScroll', wheel, false);
+  }
+  window.onmousewheel = document.onmousewheel = wheel;
+  document.onkeydown = keydown;
+}
+
+disableScroll();
