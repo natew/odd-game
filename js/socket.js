970a78bc182850934fd87fd323984614f80a979a
DeShawn Williams <deswilliams@zappos.com>
2013-12-05 22:18:28 -0800

    test shot

--- js/socket.js
+++ js/socket.js
@@ -8,3 +8,8 @@ function setData(data) {
   DATA_PREV[data.id] = DATA;
   DATA[data.id] = data;
 }
+
+var socket2 = io.connect('http://localhost:9002');
+socket2.on('message', function (data2) {
+  shoot();
+});
